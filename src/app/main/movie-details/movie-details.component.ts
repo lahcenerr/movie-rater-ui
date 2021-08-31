7a6a88279e76fe791ce68a8bfabce02ec252f170
lahcen <lahcenerrich@gmail.com>
2021-08-30 23:41:11 -0400

    rates

--- src/app/main/movie-details/movie-details.component.ts
+++ src/app/main/movie-details/movie-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input,Output, EventEmitter, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
+import { Movie } from 'src/app/models/movie';
 
 
 @Component({
@@ -10,19 +11,30 @@ import { ApiService } from 'src/app/api.service';
 export class MovieDetailsComponent implements OnInit {
 
   @Input() movie: any;
+  @Output() updatedMovie = new EventEmitter();
   rateHovered = 0;
   constructor( private apiService: ApiService) { }
 
   ngOnInit(): void {
   }
-  rateMovie(rate: any){
+  rateMovie(rate: number){
     this.rateHovered = rate;
   }
-  rateClicked(rate: any){
+  rateClicked(rate: number){
     console.log(rate);
     this.apiService.rateMovie(rate, this.movie.id).subscribe(
-      
-    )
+      result => this.getDetails(),
+      error => console.log(error)
+    );
+  }
+
+  getDetails(){
+    this.apiService.getMovie(this.movie.id).subscribe(
+      movie => {
+        this.updatedMovie.emit(movie);
+      },
+      error => console.log(error)
+      );
   }
 
 }
